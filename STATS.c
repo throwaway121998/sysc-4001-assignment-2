5b07697721d31eb83995d5ed50a12e3a85130aa3
throwaway121998 <47678330+throwaway121998@users.noreply.github.com>
2019-03-18 14:15:31 -0400

    Update STATS.c

--- STATS.c
+++ STATS.c
@@ -13,23 +13,17 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 
+#include <sys/shm.h>
+#include <sys/types.h>
+
 #include "STATS.h"
 #include "semun.h"
 
-static int set_semvalue(void);
-static void del_semvalue(void);
-static int semaphore_p(void);
-static int semaphore_v(void);
-
-static int sem_id;
+static int set_semvalue(int sem_id);
+static int semaphore_p(int sem_id);
+static int semaphore_v(int sem_id);
 
 int main(int argc, const char * argv[]) {
-    /* Instantiate the semaphore. */
-    sem_id = semget((key_t)1234, 1, 0666 | IPC_CREAT);
-    if (!set_semvalue()) {
-        fprintf(stderr, "Failed to initialize the semaphore\n");
-        exit(EXIT_FAILURE);
-    }
     /* Instantiate the shared memory for multiple processes. */
     void *shared_memory = (void *)0;
     struct shared_data *shared_data;
@@ -47,13 +41,19 @@ int main(int argc, const char * argv[]) {
         exit(EXIT_FAILURE);
     }
     printf("The shared mem is attached @ location: 0x%X\n", (int)shared_memory);
-    /* Instantiate the array from user input. */
+    
     shared_data = (struct shared_data *)shared_memory;
     for(int i = 0; i < SZ; i++) {
+        /* Instantiate the array from user input. */
         printf("Enter a # for arr[%d]: ", i);
         scanf("%d", &shared_data->B[i]);
+        /* Instantiate the semaphores. */
+        shared_data->mutex[i] = semget((key_t)i /* A unique non-zero key. */, 1, 0666 | IPC_CREAT);
+        if (!set_semvalue(shared_data->mutex[i])) {
+            fprintf(stderr, "Failed to initialize the semaphore\n");
+            exit(EXIT_FAILURE);
+        }
     }
-    shared_data->is_alive = 1;
     /* Fork SZ - 1 processes. */
     int n_processes = SZ - 1;
     for(int i = 0; i < n_processes; i++) {
@@ -64,46 +64,56 @@ int main(int argc, const char * argv[]) {
                 exit(EXIT_FAILURE);
             case 0:
                 /* Child process. */
-                while(shared_data->is_alive) {
-                    /* Enter the critical section. */
-                    if (!semaphore_p()) {
-                        exit(EXIT_FAILURE);
-                    }
-                    /* Swap the elements of the array if and only if arr[i] < arr[i + 1] */
-                    if (shared_data->B[i] < shared_data->B[i + 1]) {
-                        int temp = shared_data->B[i];
-                        shared_data->B[i] = shared_data->B[i + 1];
-                        shared_data->B[i + 1] = temp;
-                    }
-                    /* Exit the critical section. */
-                    if (!semaphore_v()) {
-                        exit(EXIT_FAILURE);
+                for(int i = 0; i < SZ - 1; i++) {
+                    /* Asymmetric solution. */
+                    if (i % 2 == 0) {
+                        /* Enter the critical section. */
+                        if (!semaphore_p(shared_data->mutex[i])) {
+                            exit(EXIT_FAILURE);
+                        }
+                        if (!semaphore_p(shared_data->mutex[i + 1])) {
+                            exit(EXIT_FAILURE);
+                        }
+                        /* Swap the elements of the array if and only if arr[i] < arr[i + 1] */
+                        if (shared_data->B[i] < shared_data->B[i + 1]) {
+                            int temp = shared_data->B[i];
+                            shared_data->B[i] = shared_data->B[i + 1];
+                            shared_data->B[i + 1] = temp;
+                        }
+                        /* Exit the critical section. */
+                        if (!semaphore_v(shared_data->mutex[i])) {
+                            exit(EXIT_FAILURE);
+                        }
+                        if (!semaphore_v(shared_data->mutex[i + 1])) {
+                            exit(EXIT_FAILURE);
+                        }
+                    } else {
+                        /* Enter the critical section. */
+                        if (!semaphore_p(shared_data->mutex[i + 1])) {
+                            exit(EXIT_FAILURE);
+                        }
+                        if (!semaphore_p(shared_data->mutex[i])) {
+                            exit(EXIT_FAILURE);
+                        }
+                        /* Swap the elements of the array if and only if arr[i] < arr[i + 1] */
+                        if (shared_data->B[i] < shared_data->B[i + 1]) {
+                            int temp = shared_data->B[i];
+                            shared_data->B[i] = shared_data->B[i + 1];
+                            shared_data->B[i + 1] = temp;
+                        }
+                        /* Exit the critical section. */
+                        if (!semaphore_v(shared_data->mutex[i + 1])) {
+                            exit(EXIT_FAILURE);
+                        }
+                        if (!semaphore_v(shared_data->mutex[i])) {
+                            exit(EXIT_FAILURE);
+                        }
                     }
                 }
-                /* Exit the process. */
                 exit(0);
                 break;
             default:
                 /* Parent process. */
-                while (shared_data->is_alive) {
-                    int is_ok = 1;
-                    for(int i = 0; i < SZ; i++) {
-                        /* Enter the critical section. */
-                        if (!semaphore_p()) {
-                            exit(EXIT_FAILURE);
-                        }
-                        if (shared_data->B[i] < shared_data->B[i + 1]) {
-                            is_ok = 0;
-                        }
-                        /* Exit the critical section. */
-                        if (!semaphore_v()) {
-                            exit(EXIT_FAILURE);
-                        }
-                    }
-                    if (is_ok) {
-                        shared_data->is_alive = 0;
-                    }
-                }
                 break;
         }
     }
@@ -118,15 +128,13 @@ int main(int argc, const char * argv[]) {
         if (i == SZ - 1) {
             printf("%d]\n", shared_data->B[i]);
         } else {
-            printf("%d, ", shared_data->B[i]);
+            printf("%d, ",  shared_data->B[i]);
         }
     }
     /* Print the min, median and max elements in the array. */
     printf("Min value: %d\n", shared_data->B[SZ - 1]);
     printf("Median value: %d\n", shared_data->B[SZ / 2]);
     printf("Max value: %d\n", shared_data->B[0]);
-    /* Delete the semaphore. */
-    del_semvalue();
     /* Detach and delete the shared memory. */
     if (shmdt(shared_memory) == -1) {
         fprintf(stderr, "shmdt failed\n");
@@ -139,7 +147,7 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-static int set_semvalue(void) {
+static int set_semvalue(int sem_id) {
     union semun sem_union;
     sem_union.val = 1;
     if (semctl(sem_id, 0, SETVAL, sem_union) == -1) {
@@ -148,14 +156,7 @@ static int set_semvalue(void) {
     return(1);
 }
 
-static void del_semvalue(void) {
-    union semun sem_union;
-    if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1) {
-        fprintf(stderr, "Failed to delete the semaphore\n");
-    }
-}
-
-static int semaphore_p(void) {
+static int semaphore_p(int sem_id) {
     struct sembuf sem_b;
     sem_b.sem_num = 0;
     sem_b.sem_op = -1;
@@ -167,7 +168,7 @@ static int semaphore_p(void) {
     return(1);
 }
 
-static int semaphore_v(void) {
+static int semaphore_v(int sem_id) {
     struct sembuf sem_b;
     sem_b.sem_num = 0;
     sem_b.sem_op = 1;
